28e94e0b521769af57e1819b5b7d39c3d3db4a4e
leonardo arena <leonardofarena@gmail.com>
2023-12-16 12:46:49 -0300

    nn

--- controllers/productos.js
+++ controllers/productos.js
@@ -3,62 +3,68 @@ const Producto = require('../models/producto');
 const { response } = require('express');
 const { generarJWT } = require('../helpers/jwt');
 
-const getProductos = async(req, res) => {      
-        
-    const desde = Number(req.query.desde) || 0;      
+const getProductos = async (req, res) => {
+
+    const desde = Number(req.query.desde) || 0;
+    await Promise.all([
+        Producto.countDocuments(),
+        Producto.find().populate('usuario', 'nombre img telefono')
+            .populate('categoria', 'nombre img')
+            .skip(desde)
+            .limit(12)
+    ])
+        .then(respuestas => {
+            res.json({
+                ok: true,
+                productos: respuestas[1],
+                total: respuestas[0]
+            });
+        });
+}
+
+
+
+const getProductosById = async (req, res) => {
+    const id = req.params.id;
+
+    try {
         await Promise.all([
             Producto.countDocuments(),
-            Producto.find().populate('usuario', 'nombre img telefono')
-                          .populate('categoria', 'nombre img')
-                .skip( desde )
-                .limit( 12 )                             
-            ])
-        .then( respuestas => {                
+            Producto.findById(id)
+                .populate('usuario', 'nombre img telefono')
+                .populate('categoria', 'nombre img')
+        ])
+            .then(respuestas => {
                 res.json({
                     ok: true,
                     productos: respuestas[1],
-                    total: respuestas[0]
-                });        
-            });            
-        }
-
-        
-
-        const getProductosById = async(req, res) => {    
-            const id = req.params.id;         
-            
-            try {    
-            await Promise.all([
-                Producto.countDocuments(),
-                Producto.findById(id)
-                             .populate('usuario', 'nombre img telefono')
-                              .populate('categoria', 'nombre img')                  
-            ])
-            .then( respuestas => {                    
-                    res.json({
-                        ok: true,
-                        productos: respuestas[1],                    
-                    });            
                 });
+            });
 
-            } catch (error) {
-                console.log(error);
-                res.status(500).json({
-                    ok: false,
-                    msg: 'Hable con el administrador',
-                    error: error        
-                });        
-            }
-     }
-        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador',
+            error: error
+        });
+    }
+}
 
-const crearProducto = async(req, res = response) => {
+
+const crearProducto = async (req, res = response) => {
     const producto = new Producto({
         usuario: req.uid,
         ...req.body
     });
-
     try {
+        const existeProducto = await Producto.findOne({ nombre: producto.nombre });
+        if (existeProducto) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El nombre ya está registrado, ingrese otro nombre'
+            });
+        }
         const productoDB = await producto.save();
         res.json({
             ok: true,
@@ -72,11 +78,11 @@ const crearProducto = async(req, res = response) => {
             msg: 'Hable con el administrador'
         });
 
-    }  
+    }
 
 }
 
-const actualizarProducto = async(req, res = response) => {
+const actualizarProducto = async (req, res = response) => {
 
     const uid = req.params.id;
     try {
@@ -119,90 +125,90 @@ const actualizarProducto = async(req, res = response) => {
         });
     }
 
-    }
+}
 
-    const actualizarLinkProducto = async(req, res = response) => {
-            
-            const uid = req.params.id;
-            try {
-                const productoDB = await Producto.findById(uid);
-                if (!productoDB) {
-                    return res.status(404).json({
-                        ok: false,
-                        msg: 'No existe una producto con ese id'
-                    });
-                }
-        
-                // Actualizaciones
-        
-                const { linkdepago, ...campos } = req.body;
+const actualizarLinkProducto = async (req, res = response) => {
 
-                if(linkdepago == ''){
-                    campos.linkdepago = linkdepago;        
-                    const productoActualizado = await Producto.findByIdAndUpdate(uid, campos, { new: true }); 
-                    return res.json({
-                        ok: true,
-                        producto: productoActualizado
-                    });
-                  
-                }               
-        
-                campos.linkdepago = linkdepago;
-        
-                const productoActualizado = await Producto.findByIdAndUpdate(uid, campos, { new: true });
-        
-                res.json({
-                    ok: true,
-                    producto: productoActualizado
-                });
-            }
-        
-            catch (error) {
-                console.log(error);
-                res.status(500).json({
-                    ok: false,
-                    msg: 'Hable con el administrador'
-                });
-            }
-        
-            }
+    const uid = req.params.id;
+    try {
+        const productoDB = await Producto.findById(uid);
+        if (!productoDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe una producto con ese id'
+            });
+        }
 
-const actualizarEstadoProducto = async(req, res = response) => {    
-        const uid = req.params.id;
-        try {
-            const productoDB = await Producto.findById(uid);
-            if (!productoDB) {
-                return res.status(404).json({
-                    ok: false,
-                    msg: 'No existe una producto con ese id'
-                });
-            }
-    
-            // Actualizaciones
-    
-            const { estado, ...campos } = req.body;
-    
-            campos.estado = estado;
-    
+        // Actualizaciones
+
+        const { linkdepago, ...campos } = req.body;
+
+        if (linkdepago == '') {
+            campos.linkdepago = linkdepago;
             const productoActualizado = await Producto.findByIdAndUpdate(uid, campos, { new: true });
-    
-            res.json({
+            return res.json({
                 ok: true,
                 producto: productoActualizado
             });
-        }
-    
-        catch (error) {
-            console.log(error);
-            res.status(500).json({
-                ok: false,
-                msg: 'Hable con el administrador'
-            });
-        }
-    
+
         }
 
-const borrarProducto = async(req, res = response) => {
+        campos.linkdepago = linkdepago;
+
+        const productoActualizado = await Producto.findByIdAndUpdate(uid, campos, { new: true });
+
+        res.json({
+            ok: true,
+            producto: productoActualizado
+        });
+    }
+
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
+}
+
+const actualizarEstadoProducto = async (req, res = response) => {
+    const uid = req.params.id;
+    try {
+        const productoDB = await Producto.findById(uid);
+        if (!productoDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe una producto con ese id'
+            });
+        }
+
+        // Actualizaciones
+
+        const { estado, ...campos } = req.body;
+
+        campos.estado = estado;
+
+        const productoActualizado = await Producto.findByIdAndUpdate(uid, campos, { new: true });
+
+        res.json({
+            ok: true,
+            producto: productoActualizado
+        });
+    }
+
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
+}
+
+const borrarProducto = async (req, res = response) => {
 
     const uid = req.params.id;
     try {
@@ -232,24 +238,24 @@ const borrarProducto = async(req, res = response) => {
 
         });
 
-    }    
+    }
 
 }
 
-const getProductosByCategoria = async(req, res = response) => {
+const getProductosByCategoria = async (req, res = response) => {
     const categoria = req.params.categoria;
     const desde = Number(req.query.desde) || 0;
     try {
         const [productos, total] = await Promise.all([
             Producto.find({ categoria }).populate('usuario', 'nombre img telefono')
-            .populate('categoria', 'nombre img')
-            .skip( desde )
-            .limit( 12 ),
-            Producto.countDocuments({ categoria })                
+                .populate('categoria', 'nombre img')
+                .skip(desde)
+                .limit(12),
+            Producto.countDocuments({ categoria })
         ]);
         res.json({
             ok: true,
-            productos,            
+            productos,
             total
         });
 
